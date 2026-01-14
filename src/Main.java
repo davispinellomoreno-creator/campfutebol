4a855d0f19364e56a4c58c06ddd0907843356b8c
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-01-14 14:35:27 -0300

    Pequenas Alterações

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,27 @@ public class Main {
         times.add(new Time("Fluminense", 0, 0, 0, 0));
         times.add(new Time("Palmeiras", 0, 0, 0, 0));
         times.add(new Time("Corinthians", 0, 0, 0, 0));
+        times.add(new Time("Vitória", 0, 0, 0, 0));
+        times.add(new Time("Bahia", 0, 0, 0, 0));
+        times.add(new Time("Remo", 0, 0, 0, 0));
+        times.add(new Time("Chapecoense", 0, 0, 0, 0));
+        times.add(new Time("Mirassol", 0, 0, 0, 0));
+        times.add(new Time("Cruzeiro", 0, 0, 0, 0));
+        times.add(new Time("São Paulo", 0, 0, 0, 0));
+        times.add(new Time("Santos", 0, 0, 0, 0));
+        times.add(new Time("Atlético MG", 0, 0, 0, 0));
+        times.add(new Time("Coritiba", 0, 0, 0, 0));
+        times.add(new Time("Athletico PR", 0, 0, 0, 0));
+        times.add(new Time("Botafogo", 0, 0, 0, 0));
+        times.add(new Time("Vasco da Gama", 0, 0, 0, 0));
+        times.add(new Time("Grêmio", 0, 0, 0, 0));
+        times.add(new Time("Athletico PR", 0, 0, 0, 0));
+        times.add(new Time("Bragantino", 0, 0, 0, 0));
+        times.add(new Time("Internacional", 0, 0, 0, 0));
+        times.add(new Time("Coritiba", 0, 0, 0, 0));
+        times.add(new Time("Athletico PR", 0, 0, 0, 0));
+        times.add(new Time("Botafogo", 0, 0, 0, 0));
+
 
         System.out.println("Times participantes da Supercopa Brasileira:");
         for (Time time : times) {
